a8095a68d8ebedc1968c88474e671de129e43b66
Krunoslav Kasalo <72451022+kkruno25@users.noreply.github.com>
2022-11-19 17:50:39 +0100

    Create zupanijaopcina.sql

---
+++ zupanijaopcina.sql
@@ -0,0 +1,21 @@
+# Otvoriti CMD
+# Unijeti D:\Programiranje\Xampp\mysql\bin\mysql -uroot < D:\Programiranje\javavjezbanje\edunova\SQL\zupanijaopcina.sql
+
+drop database if exists zupanijaopcina;
+create database zupanijaopcina;
+use zupanijaopcina;
+
+create table opcina(
+    sifra int not null primary key auto_increment,
+    zupanija int,
+    naziv varchar(50)
+);
+
+create table zupanija(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    zupan varchar(50) 
+);
+
+# Vanjski kljucevi
+alter table opcina add foreign key (zupanija) references zupanija (sifra);
