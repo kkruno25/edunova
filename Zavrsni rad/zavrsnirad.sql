35cf89eb278f66ea874235efd7fb1b60f8e5ba03
Krunoslav Kasalo <72451022+kkruno25@users.noreply.github.com>
2022-12-09 15:54:31 +0100

    promjena zbog drukcijeg ERA dijagrama

--- Zavrsni rad/zavrsnirad.sql
+++ Zavrsni rad/zavrsnirad.sql
@@ -12,29 +12,11 @@ create table vozilo(
     tipvozila varchar(50)
 );
 
-create table vozac(
-    sifra int not null primary key auto_increment,
-    ime varchar(50),
-    prezime varchar(50),
-    brojmobitela char(10)
-);
-
-create table kupac(
-    sifra int not null primary key auto_increment,
-    ime varchar(50),
-    prezime varchar(50),
-    adresa varchar(50),
-    oib char(11),
-    brojmobitela char(10),
-    email varchar(50)
-);
-
 create table gradiliste(
     sifra int not null primary key auto_increment,
     adresa varchar(50),
-    narucenakolicinabetona char(5),
-    nacinistovara varchar(50),
-    kupac int
+    narucenakolicinabetona decimal(5,2),
+    nacinistovara varchar(50)
 );
 
 create table odvoz(
@@ -43,24 +25,22 @@ create table odvoz(
     dolazaknagradiliste datetime,
     pocetakistovara datetime,
     krajistovara datetime,
-    vozilo int,
-    vozac int
+    kolicinabetona decimal(2,2),
+    vozilo int
 );
 
 create table proizvodnja(
     sifra int not null primary key auto_increment,
     pocetakproizvodnje datetime,
     krajproizvodnje datetime,
-    proizvedenakolicina decimal(1,1),
-    razredcvrstoce varchar(6),
+    proizvedenakolicina decimal(2,2),
+    tlacnacvrstoca varchar(6),
     gradiliste int,
     odvoz int
 );
 
 # Vanjski ključevi
 alter table odvoz add foreign key (vozilo) references vozilo(sifra);
-alter table odvoz add foreign key (vozac) references vozac(sifra);
-alter table gradiliste add foreign key (kupac) references kupac(sifra);
 alter table proizvodnja add foreign key (gradiliste) references gradiliste(sifra);
 alter table proizvodnja add foreign key (odvoz) references odvoz(sifra);
 
